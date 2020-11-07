94163c26dd0b0c1e06e36b79af2d7a305196aee7
britnijgrimm <70454567+britnijgrimm@users.noreply.github.com>
2020-11-07 11:02:49 -0800

    "Adding queries.sql."

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -13,37 +13,31 @@ CREATE TABLE employees (
 	last_name VARCHAR NOT NULL,
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE salaries (
-  emp_no INT NOT NULL,
-  salary INT NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
-
 CREATE TABLE dept_emp (
+	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
-	dept_no VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
+
+ALTER TABLE dept_emp DROP CONSTRAINT dept_emp_pkey;
 
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
@@ -54,4 +48,36 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
 
+ALTER TABLE titles DROP CONSTRAINT titles_pkey;
+
+CREATE TABLE salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+ 	PRIMARY KEY (emp_no)
+);
+
+--Drop tables and relationships
+DROP TABLE departments CASCADE;
+DROP TABLE employees CASCADE;
+DROP TABLE dept_manager CASCADE;
+DROP TABLE dept_emp CASCADE;
+DROP TABLE titles CASCADE;
+DROP TABLE salaries CASCADE;
+
+--Review imported data
 SELECT * FROM departments;
+
+SELECT * FROM employees;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM titles;
+
+--SELECT MAX(emp_no) FROM the_table;
+--SELECT nextval('the_primary_key_sequence');
+--SELECT pg_get_serial_sequence('the_table', 'the_primary_key')
